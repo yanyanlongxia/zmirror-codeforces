78a0a15d1a688ec300324cb71a88519f9c4ea96a
yanyanlongxia <59304287+yanyanlongxia@users.noreply.github.com>
2021-03-06 23:45:15 +0800

    Update config_codeforces.py

--- more_configs/config_codeforces.py
+++ more_configs/config_codeforces.py
@@ -14,7 +14,7 @@
 # Github: https://github.com/aploium/zmirror
 
 # ############## Local Domain Settings ##############
-my_host_name = 'codeforces.live'
+my_host_name = '127.0.0.1'
 my_host_scheme = 'http://'
 my_host_port = None  # None表示使用默认端口, 可以设置成非标准端口, 比如 81
 
